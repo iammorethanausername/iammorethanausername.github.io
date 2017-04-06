9444db95c3b244cb073730c5499acdc17d22d623
Danny Anderson <iammorethanausername@hotmail.com>
2017-04-06 04:56:40 +0100

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+var myImage = document.querySelector('img');
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+myButton.onclick = function() {
+  setUserName();
+}
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/ballroom.jpg') {
+      myImage.setAttribute ('src','images/dock.jpg');
+    } else {
+      myImage.setAttribute ('src','images/ballroom.jpg');
+    }
+}
